2d1753106954779e92666917ffa4b4e1bb84170c
PSebastian96 <>
2023-09-06 18:01:45 +0000

    navbar active class added

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
+//mobile navbar
 $(document).ready(function(){
     $('.sidenav').sidenav();
+  });
+
+// active current page
+  $(document).ready(function(){
+    $('ul li a').click(function(){
+      $('li a').removeClass("active");
+      $(this).addClass("active");
+  });
   });
