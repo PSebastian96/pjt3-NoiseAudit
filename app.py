d97daa646f224bb86cc48b0062d3532a8d798ae9
PSebastian96 <>
2023-09-06 16:05:44 +0000

    def hello world

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ app = Flask(__name__)
 
 mongo = PyMongo(app)
 
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
