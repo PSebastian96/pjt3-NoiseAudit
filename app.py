19b3467e05a458f7fdfe869eb96fe80fb8088109
PSebastian96 <>
2023-10-07 09:31:08 +0000

    heroku err fix2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-
+from flask_ckeditor import CKEditor
 if os.path.exists("env.py"):
     import env
 
